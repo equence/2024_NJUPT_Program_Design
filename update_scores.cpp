ac6e26e71f7a4f2c46b17610840616b08e1cab7a
fjq6 <1802008729@qq.com>
2024-11-14 19:16:17 +0800

    加入了院系求排名

--- update_scores.cpp
+++ update_scores.cpp
@@ -1,6 +1,20 @@
 #include "update_scores.h"
 #include <QSqlQuery>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+// 结构体用于存储院系总分和排名
+struct DepartmentScore {
+    QString departmentCode;
+    int totalScore;
+    int ranking;
+};
+
+// 比较函数用于排名
+bool compareDepartmentScores(const DepartmentScore &a, const DepartmentScore &b) {
+    return a.totalScore > b.totalScore; // 院系总分从高到低排序
+}
 
 void updateDepartmentScores() {
     // 打开数据库连接
@@ -20,18 +34,33 @@ void updateDepartmentScores() {
     )";
     QSqlQuery query(queryString);
 
-    // 更新 departments 表中的院系总分
+    // 存储每个院系的总分
+    std::vector<DepartmentScore> departmentScores;
     while (query.next()) {
-        QString departmentCode = query.value("院系编号").toString();
-        int totalScore = query.value("总分").toInt();
+        DepartmentScore score;
+        score.departmentCode = query.value("院系编号").toString();
+        score.totalScore = query.value("总分").toInt();
+        departmentScores.push_back(score);
+    }
 
+    // 对院系总分进行排序
+    std::sort(departmentScores.begin(), departmentScores.end(), compareDepartmentScores);
+
+    // 更新排名
+    for (size_t i = 0; i < departmentScores.size(); ++i) {
+        departmentScores[i].ranking = i + 1;
+    }
+
+    // 将总分和排名更新到 departments 表中
+    for (const auto &dept : departmentScores) {
         QSqlQuery updateQuery;
-        updateQuery.prepare("UPDATE departments SET 院系总分 = :totalScore WHERE 院系编码 = :departmentCode");
-        updateQuery.bindValue(":totalScore", totalScore);
-        updateQuery.bindValue(":departmentCode", departmentCode);
+        updateQuery.prepare("UPDATE departments SET 院系总分 = :totalScore, 院系排名 = :ranking WHERE 院系编码 = :departmentCode");
+        updateQuery.bindValue(":totalScore", dept.totalScore);
+        updateQuery.bindValue(":ranking", dept.ranking);
+        updateQuery.bindValue(":departmentCode", dept.departmentCode);
         updateQuery.exec();
     }
-    // 关闭数据库连接
 
+    // 关闭数据库连接
     db.close();
 }
